471ad1e0b95883f4145dac24c99b8206dfa6d6cb
takayama shunya <63427166+takayama-shunya@users.noreply.github.com>
2024-02-27 19:49:41 +0900

    基本画面構築　追加

--- lib/main.dart
+++ lib/main.dart
@@ -85,42 +85,84 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Row(
+          children: const [
+            Icon(Icons.create),
+            Text("title!!"),
+          ]
         ),
       ),
+      body: Column(children: [
+        const Text("Hello World"),
+        const Text("ハローワールド"),
+        TextButton(
+          onPressed: () => {print("click button")},
+          child: const Text("text button"),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 24.0,
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 30.0,
+            ),
+            Icon(
+              Icons.beach_access,
+              color: Colors.blue,
+              size: 36.0,
+            ),
+        ])
+      ]),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => {print("float btn clicled")},
+        child: const Icon(Icons.timer)
+      ),
+      drawer: const Drawer(child: Center(child: Text("Draweer"))),
+      endDrawer: const Drawer(child: Center(child: Text("endDrawer"))),
+      // // body: Center(
+      // //   // Center is a layout widget. It takes a single child and positions it
+      // //   // in the middle of the parent.
+      // //   child: Column(
+      // //     // Column is also a layout widget. It takes a list of children and
+      // //     // arranges them vertically. By default, it sizes itself to fit its
+      // //     // children horizontally, and tries to be as tall as its parent.
+      // //     //
+      // //     // Column has various properties to control how it sizes itself and
+      // //     // how it positions its children. Here we use mainAxisAlignment to
+      // //     // center the children vertically; the main axis here is the vertical
+      // //     // axis because Columns are vertical (the cross axis would be
+      // //     // horizontal).
+      // //     //
+      // //     // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+      // //     // action in the IDE, or press "p" in the console), to see the
+      // //     // wireframe for each widget.
+      // //     mainAxisAlignment: MainAxisAlignment.center,
+      // //     children: <Widget>[
+      // //       const Text(
+      // //         'You have pushed the button this many times:',
+      // //       ),
+      // //       Text(
+      // //         '$_counter',
+      // //         style: Theme.of(context).textTheme.headlineMedium,
+      // //       ),
+      //     ],
+        // ),
+      // ),
     );
   }
 }
+
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
